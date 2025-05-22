d6d8e1c636784692678d46ce5eb09eb300671caf
dyy <dyykntnn11@gmail.com>
2025-05-22 13:07:04 +0700

    updat title dasbprd

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app', ['title' => 'Halaman Dashboard', 'section_header' => 'Dashboard'])
 
 @section('content')
-  <h1>Selamat datang di halaman dashboard!</h1>
+  <h1>Selamat datang di halaman dashboard ini brohh!</h1>
   <p>Ini adalah contoh konten halaman dashboard.</p>
 @endsection
